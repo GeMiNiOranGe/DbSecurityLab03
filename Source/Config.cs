3ecc6540ceda7c0de78274eceea0763df27b6acf
hungSF <lehung201105@gmail.com>
2024-04-07 16:23:26 +0700

    Update Files

--- Source/Config.cs
+++ Source/Config.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace Source {
     class Config {
-        public readonly string CONNECTION_STRING = @"";
+        public static readonly string CONNECTION_STRING = @"Data Source=LAPTOP-3MHE919R\SQLSERVER22;Initial Catalog=QLSVNhom;Integrated Security=True";
     }
 }
