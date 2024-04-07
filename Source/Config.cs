0b14bf0a65e6808846411905cd9bd2f54a36ca99
TuongSieuCute <108283017+TuongSieuCute@users.noreply.github.com>
2024-04-07 12:34:19 +0700

    sửa config

--- Source/Config.cs
+++ Source/Config.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace Source {
     class Config {
-        public readonly string CONNECTION_STRING = @"";
+        public readonly string CONNECTION_STRING = @"Data Source=ADMIN;Initial Catalog=QLSVNhom;Integrated Security=True;Trust Server Certificate=True";
     }
 }
