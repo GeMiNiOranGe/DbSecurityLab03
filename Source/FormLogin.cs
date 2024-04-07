e3e158dc8804092b29acf46e46774ef975660dd9
TuongSieuCute <108283017+TuongSieuCute@users.noreply.github.com>
2024-04-07 15:54:15 +0700

    login

--- Source/FormLogin.cs
+++ Source/FormLogin.cs
@@ -36,15 +36,29 @@ namespace Source {
                 sqlCommand.Parameters.Add("@TENDN", SqlDbType.NVarChar).Value = txtDangNhap.Text;
                 sqlCommand.Parameters.Add("@MATKHAU", SqlDbType.VarChar).Value = txtMatKhau.Text;
                 sqlCommand.Parameters.Add("@KETQUA", SqlDbType.Int);
+                sqlCommand.Parameters.Add("@MANV", SqlDbType.VarChar, 100);
                 sqlCommand.Parameters["@KETQUA"].Direction = ParameterDirection.Output;
+                sqlCommand.Parameters["@MANV"].Direction = ParameterDirection.Output;
+                sqlCommand.Parameters["@MANV"].Size = 100;
 
                 sqlConnection.Open();
                 sqlCommand.ExecuteNonQuery();
 
                 int ketqua = Convert.ToInt32(sqlCommand.Parameters["@KETQUA"].Value);
+                string manv = sqlCommand.Parameters["@MANV"].Value.ToString();
                 if (ketqua == 1)
                 {
                     MessageBox.Show("Đăng nhập tài khoản thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (!string.IsNullOrEmpty(manv))
+                    {
+                        FormMenu formMenu = new FormMenu(manv);
+                        this.Hide();
+                        formMenu.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Mã nhân viên không hợp lệ", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
                 else
                 {
---
+++ Source/FormMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Source
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+        public FormMenu(string manv)
+        {
+            InitializeComponent();
+            txtMaNV.Text = manv;
+        }
+        private void btnLopHoc_Click(object sender, EventArgs e)
+        {
+            Form formClass = new FormClass();
+            formClass.ShowDialog();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
